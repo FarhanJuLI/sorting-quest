594731ef0828b3bf5b47dcb261ceb17670ed055d
FarhanJuLI <frhnjuli@gmail.com>
2026-09-24 20:15:11 +0800

    refactor: convert project from TypeScript to JavaScript

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <main>
+      <h1>Sorting Quest</h1>
+      <p>Game edukasi algoritma sorting berbasis web.</p>
+    </main>
+  )
+}
+
+export default App
